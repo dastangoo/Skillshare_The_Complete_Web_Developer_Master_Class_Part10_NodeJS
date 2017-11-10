bc3d26d4b288b187dd082cec2fde31f9ce2e42d6
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 17:50:42 +0330

    node

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+var var1 = 5;
+var var2 = 7;
+
+console.log(var1 + var2);
+
+function func1() {
+  var3 = 6;
+  var4 = 9;
+  return var3 * var4;
+}
+
+console.log(func1());
+
+function func2() {
+  console.log("Hi, this is the console, inside the funciton");
+}
+
+func2();
