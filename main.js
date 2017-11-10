9a8d4dcf45fb713ee4b09147b6d1aa4429c1e81a
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 17:56:49 +0330

    JavaScript Practices in NodeJs

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var var1 = 5;
 var var2 = 7;
+var var5 = 56;
+var var6 = 46;
+
 
 console.log(var1 + var2);
 
@@ -16,3 +19,24 @@ function func2() {
 }
 
 func2();
+
+console.log(56 == 45);
+console.log(56 == 56);
+
+if (var5 == var6) {
+  console.log("The variables are equal to each other.");
+} else {
+  console.log("The variables aren't equal to each other.");
+}
+
+for (a = 0; a < 10; a++) {
+  console.log(a + ". This is the Lopp");
+}
+
+var person = {
+  name: "John",
+  eyeColor: "Black",
+  hairColor: "Brown"
+}
+
+console.log(person.name);
