f6b2c72cc405b6d4e16b4d624c50d52b564a30cd
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 20:28:08 +0330

    adding if else and making the 404 response

--- http.js
+++ http.js
@@ -13,10 +13,19 @@
 var http = require('http');
 var fs = require('fs');
 
+function response_404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write("Sorry, the page you are looking for, is not anywhere!");
+  response.end(); 
+}
 function onRequest(request, response) {
   console.log("We got a request from the Client.");
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  fs.createReadStream('./index.html').pipe(response);
+  if (request.method == "GET" && request.url == "/") {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream('./index.html').pipe(response);    
+  } else {
+    response_404(response);
+  }
 }
 http.createServer(onRequest).listen(8081);
 console.log("The Server is working successfully");
